902e6d62935d940ae8d76444aa0089c5f7d3084a
Negar Baharmand <negarbaharmand@outlook.com>
2024-09-17 09:38:12 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/g51springbootjpa/G51SpringBootJpaApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.g51springbootjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G51SpringBootJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(G51SpringBootJpaApplication.class, args);
+    }
+
+}
