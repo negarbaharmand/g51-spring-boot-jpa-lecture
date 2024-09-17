02f3e032ac31ae8c2929bd9a5098b09d0a1b27ec
Negar Baharmand <negarbaharmand@outlook.com>
2024-09-17 11:32:22 +0200

    Create Student and Address entities using JPA annotations and add property file for MySQL database

---
+++ src/main/java/se/lexicon/g51springbootjpa/entity/Address.java
@@ -0,0 +1,32 @@
+package se.lexicon.g51springbootjpa.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String street;
+    @Column(nullable = false)
+    private String city;
+    @Column(nullable = false, length = 6)
+    private String zipCode;
+
+    public Address(String street, String city, String zipCode) {
+        this.street = street;
+        this.city = city;
+        this.zipCode = zipCode;
+    }
+}
---
+++ src/main/java/se/lexicon/g51springbootjpa/entity/Student.java
@@ -0,0 +1,55 @@
+package se.lexicon.g51springbootjpa.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+//@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@ToString //(exclude = "createDate")
+//@Data
+
+@Entity //(name = "_students")
+//@Table(name = " ")
+public class Student {
+
+    @Id
+    //@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(nullable = false, length = 100)
+    @Setter
+    private String firstName;
+
+    @Column(nullable = false, length = 100)
+    @Setter
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    @Setter
+    private String email;
+
+    @Setter
+    private boolean status;
+
+    private LocalDateTime createDate;
+
+
+    @Setter
+    @OneToOne
+    @JoinColumn(name = "address_id")
+    private Address address;
+
+    public Student(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.status = true;
+        this.createDate = LocalDateTime.now();
+    }
+}
